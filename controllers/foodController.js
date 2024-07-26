6e3615f098c23ac0a1814dbd79c3f015c1e505e3
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-07-26 09:20:19 +0600

    added logics to add a food in the food controller file

--- controllers/foodController.js
+++ controllers/foodController.js
@@ -4,8 +4,28 @@ import fs from "fs";
 // food adding api
 
 const addFood = async (req, res) => {
-    
+
+    let image_fileName = `${req.file.filename}`;
+
+    const food = new foodModel({
+        name: req.body.name,
+        description: req.body.description,
+        price: req.body.price,
+        category: req.body.category,
+        image: image_fileName
+    })
+
+    try {
+        await food.save();
+        res.json({ success: true, message: "Food added" })
+    }
+    catch (error) {
+        console.log("error :-", error);
+        res.json({ success: false, message: "Error" })
+    }
+
+
 };
 
 
-export {addFood};
+export { addFood };
