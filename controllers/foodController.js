eb4cda6942488cad87f1b37fa0d0e3d52cb39345
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-07-26 10:54:11 +0600

    added functionality to delete a data from the database

--- controllers/foodController.js
+++ controllers/foodController.js
@@ -34,9 +34,23 @@ const listFood = async (req, res) => {
         res.json({ success: true, data: foods })
     } catch (error) {
         console.log(error);
-        res.json({success:false, message:"Error from listFood fn"})
+        res.json({ success: false, message: "Error from listFood fn" })
+    }
+}
+
+//remove food item
+const removeFood = async (req, res) => {
+    try {
+        const food = await foodModel.findById(req.body.id);
+        fs.unlink(`uploads/${food.image}`, () => { })
+
+        await foodModel.findByIdAndDelete(req.body.id);
+        res.json({ success: true, message: "Food removed" })
+    } catch (error) {
+        console.log(error)
+        res.json({success:false,message:"Error in removeFood fn"})
     }
 }
 
 
-export { addFood, listFood };
+export { addFood, listFood, removeFood };
