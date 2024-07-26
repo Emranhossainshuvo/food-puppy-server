cf31903fdd61cc1547ea6709b7bd94d6d35f4c46
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-07-26 07:33:43 +0600

    food model component created and created food scema and food model

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 import express from "express";
 import cors from "cors";
 import { connectDB } from "./config/db.js";
-
 const app = express();
 const port = 4000;
 
-
+// middlewares
 app.use(express.json());
 app.use(cors())
 
+// database connection
 connectDB();
 
 app.get("/", (req, res) => {
