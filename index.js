c341a30f9b0953be4680710dd00064eea18af87d
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-07-28 06:52:25 +0600

    user router endpoint added in the index.js file

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import { connectDB } from "./config/db.js";
 import foodRouter from "./routes/foodRoute.js";
+import userRouter from "./routes/userRoute.js";
 const app = express();
 const port = 4000;
 
@@ -13,8 +14,9 @@ app.use(cors())
 connectDB();
 
 // api endpoints
-app.use("/api/food", foodRouter)
-app.use("/images", express.static('uploads'))
+app.use("/api/food", foodRouter);
+app.use("/images", express.static('uploads'));
+app.use("/api/user",userRouter)
 
 app.get("/", (req, res) => {
     res.send("Backend server is working!!")
