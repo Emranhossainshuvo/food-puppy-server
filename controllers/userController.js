b9802135605929b6d77f5fd0668bed179db4e3eb
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-07-28 07:24:38 +0600

    user email validation added and hashed the password by bcrypt

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,9 +8,41 @@ const loginUser = async (req, res) => {
 
 };
 
+// token creation
+
 // register user 
 const registerUser = async (req, res) => {
+    const { name, email, password } = req.body || {};
+    try {
+        // Checking if the user already exists
+        const exists = await userModel.findOne({ email });
+        if (exists) {
+            return res.json({ success: false, message: "User already exists" });
+        }
+        // email format and strong password validation
+        if (!validator.isEmail(email)) {
+            return res.json({ success: false, message: "Please enter a valid email" })
+        };
 
+        if (password.length < 6) {
+            return res.json({ success: false, message: "Password must be 6 digits of more!!" })
+        }
+
+        // hashing user password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new userModel({
+            name:name,
+            email:email,
+            password:hashedPassword
+        });
+
+        const user = await newUser.save();
+
+    } catch (error) {
+
+    }
 };
 
-export {loginUser, registerUser};
+export { loginUser, registerUser };
