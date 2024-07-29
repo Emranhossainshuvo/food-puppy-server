7d13dc6f1d218a56b544a77127d56aabc0f38bbd
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-07-29 08:49:06 +0600

    created two more function to remove from cart and get the cart and exported all of them

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,6 +1,19 @@
-import userModel from "../models/userModel.js"; 
+import userModel from "../models/userModel.js";
 
 // add to cart function for users
 const addToCart = async (req, res) => {
+
+};
+
+// remove from cart function for users
+const removeFromCart = async (req, res) => {
+
+}
+
+// get cart function for users
+const getCart = async (req, res) => {
     
 }
+
+
+export {addToCart, removeFromCart, getCart}
