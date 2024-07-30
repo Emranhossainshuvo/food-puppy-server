0f21c337c7b4ea026a2d3f0f25db5228ee372b56
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-07-30 21:15:43 +0600

    created order controller file and imported necessary packages and created placeorder function

---
+++ controllers/orderController.js
@@ -0,0 +1,9 @@
+import orderModel from "../models/orderModel.js";
+import userModel from "../models/orderModel.js";
+import stripe from "stripe";
+
+
+const placeOrder = async (req, res) => {
+    
+}
+
