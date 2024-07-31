dc90d6f86db94a928cf1b8211ac781d3543451a6
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-07-31 15:30:54 +0600

    return error if catch block catches anything to the client side ligic added

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -23,35 +23,36 @@ const placeOrder = async (req, res) => {
             price_data: {
                 currency: "inr",
                 product_data: {
-                    name:item?.name
+                    name: item?.name
                 },
-                unit_amount:item?.price*100*80
+                unit_amount: item?.price * 100 * 80
             },
-            quantity:item?.quantity
+            quantity: item?.quantity
         }));
 
         line_items.push({
-            price_data:{
-                currency:"inr",
-                product_data:{
-                    name:"Delivery Charges"
+            price_data: {
+                currency: "inr",
+                product_data: {
+                    name: "Delivery Charges"
                 },
-                unit_amount:2*100*80
+                unit_amount: 2 * 100 * 80
             },
-            quantity:1
+            quantity: 1
         })
 
         const session = await stripe.checkout.sessions.create({
-            line_items:line_items,
-            mode:"payment",
-            success_url:`${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
-            cancel_url:`${frontend_url}/verify?success=false&orderId=${newOrder._id}`,
+            line_items: line_items,
+            mode: "payment",
+            success_url: `${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
+            cancel_url: `${frontend_url}/verify?success=false&orderId=${newOrder._id}`,
         })
 
-        res.json({success:true,success_url:session?.url})
+        res.json({ success: true, success_url: session?.url })
 
     } catch (error) {
-
+        console.log(error);
+        res.json({ success: false, message: "Something went wrong!!" })
     }
 };
 
