5705dba7e4b184dda85eb78ab18961c10dd5b51c
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-07-31 15:26:21 +0600

    redirecting to the frontend success page created with the url

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -5,6 +5,9 @@ import Stripe from "stripe";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 const placeOrder = async (req, res) => {
+
+    const frontend_url = "http://localhost:5173"
+
     try {
         const newOrder = new orderModel({
             userId: req.body.userId,
@@ -38,7 +41,12 @@ const placeOrder = async (req, res) => {
             quantity:1
         })
 
-        const session = await stripe.checkout.sessions.create()
+        const session = await stripe.checkout.sessions.create({
+            line_items:line_items,
+            mode:"payment",
+            success_url:`${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
+            cancel_url:`${frontend_url}/verify?success=false&orderId=${newOrder._id}`,
+        })
 
     } catch (error) {
 
