0f1f5bc16077942e4428eb5e591d6c2855e8c1b4
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-08-07 11:27:28 +0600

    create function to write the logic for update status and created route for that function

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -84,7 +84,17 @@ const userOrders = async (req, res) => {
 }
 
 const listOrders = async (req, res) => {
+    try {
+        const orders = await orderModel.find({});
+        res.json({ success: true, data: orders })
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: "Something went wrong!!" })
+    }
+}
+
+const updateStatus = async (req, res) => {
     
 }
 
-export { placeOrder, verifyOrder, userOrders, listOrders };
+export { placeOrder, verifyOrder, userOrders, listOrders, updateStatus };
